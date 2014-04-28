f58671ad9529027dac6e70e92078bb792819fedb
uupaa <uupaa.js@gmail.com>
2014-04-28 09:24:58 +0900

    follow NodeModule.js

--- test/index.node.js
+++ test/index.node.js
@@ -1,8 +1,8 @@
-require("uupaa.valid.js");
-require("uupaa.task.js");
-require("uupaa.test.js");
-require("uupaa.bytearray.js");
+require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
+require("../node_modules/uupaa.valid.js/lib/Valid.js");
+require("../node_modules/uupaa.task.js/lib/Task.js");
+require("../node_modules/uupaa.test.js/lib/Test.js");
+require("../node_modules/uupaa.bytearray.js/lib/ByteArray.js");
 require("../lib/MD5.js");
 require("../release/MD5.min.js");
 require("./test.js");
-
