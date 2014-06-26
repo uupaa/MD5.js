e1536dfaa2bedee3e54e3cc55ed4be6069d6b731
uupaa <uupaa.js@gmail.com>
2014-06-27 05:48:32 +0900

    use TypedArray

--- test/test.js
+++ test/test.js
@@ -29,9 +29,10 @@ function testMD5Binary(next) {
 
     var source = "aaa";
     var answer = "47bce5c74f589f4867dbd57e9ca9f808";
-    var md5HashArray = MD5.encode(DataType["Array"].fromString(source));
+    var md5HashArray = MD5.encode(DataType["Uint8Array"].fromString(source));
+    var array = Array.prototype.slice.call(md5HashArray);
 
-    var match = md5HashArray.every(function(value, index) {
+    var match = array.every(function(value, index) {
             var hex = parseInt(answer.slice(index * 2, index * 2 + 2), 16);
 
             return value === hex;
